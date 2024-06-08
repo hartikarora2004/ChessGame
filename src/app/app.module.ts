2da7fd2c36170fa188f396cff2620da9fd89cc4b
hartikarora2004 <hartikarora2502@gmail.com>
2024-06-08 05:47:48 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+// import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChessBoardComponent } from './modules/chess-board/chess-board.component';
+import { ComputerModeComponent } from './modules/computer-mode/computer-mode.component';
+import { NavMenuComponent } from './modules/nav-menu/nav-menu.component';
+import { PlayAgainstComputerDialogComponent } from './modules/play-against-computer-dialog/play-against-computer-dialog.component';
+import { MoveListComponent } from './modules/move-list/move-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChessBoardComponent,
+    ComputerModeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavMenuComponent,
+    PlayAgainstComputerDialogComponent,
+    MoveListComponent,
+  
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/move-list/move-list.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MoveList } from '../../chess-logic/models';
+
+@Component({
+  selector: 'app-move-list',
+  templateUrl: './move-list.component.html',
+  styleUrl: './move-list.component.css',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, CommonModule]
+})
+export class MoveListComponent {
+  @Input({ required: true }) public moveList!: MoveList;
+  @Input({ required: true }) public gameHistoryPointer: number = 0;
+  @Input({ required: true }) public gameHistoryLength: number = 1;
+  @Output() public showPreviousPositionEvent = new EventEmitter<number>();
+
+  public showPreviousPosition(moveIndex: number): void {
+    this.showPreviousPositionEvent.emit(moveIndex);
+  }
+}
